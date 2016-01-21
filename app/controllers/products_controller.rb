e56a8b0c2d5e8173b254d81c1539c1976a855de3
willienn8254 <willienn@hotmail.com>
2016-01-21 11:05:42 -0500

    Crea la ruta path /products/:id

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -43,6 +43,21 @@ class ProductsController < ApplicationController
 
 	end
 
+	def update
+
+		@product=Product.find(params[:id])
+
+		if @product.update(product_params)
+		redirect_to products_path, notice: "El producto ha sido modificaco con exito"
+
+		else
+
+			render :edit
+		end
+
+
+	end
+
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   post '/products', to: 'products#create'
   get '/products/:id', to: 'products#show', as: 'product'
   get '/products/:id/edit', to: 'products#edit'
+  patch 'products/:id', to: 'products#update'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

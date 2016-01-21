2f58f5829c477bd454eb37614783928c002735dd
willienn8254 <willienn@hotmail.com>
2016-01-21 17:31:41 -0500

    Implementa la funcionalida de autenticarse

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
 
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+
   resources :users , only: [:new, :create]
   #get '/products', to: 'products#index'
   #get '/products/new', to: 'products#new', as: 'new_product'

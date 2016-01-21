d5ea864053e0e1054ae530d725356e2ebf163dec
willienn8254 <willienn@hotmail.com>
2016-01-21 11:13:28 -0500

    Agrega enlace para editar productos

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/products/new', to: 'products#new', as: 'new_product'
   post '/products', to: 'products#create'
   get '/products/:id', to: 'products#show', as: 'product'
-  get '/products/:id/edit', to: 'products#edit'
+  get '/products/:id/edit', to: 'products#edit', as: 'edit_product'
   patch 'products/:id', to: 'products#update'
 
   # The priority is based upon order of creation: first created -> highest priority.
